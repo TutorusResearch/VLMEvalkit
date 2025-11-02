18e7eb19215c3c979c403b22a97e3d568247bc98
DongJae Shin <djshin1998@tutoruslabs.com>
2025-11-03 00:00:51 +0900

    fixed dataset name

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 torchrun --nproc-per-node=2 run.py \
-    --data GSM8K_KOR \
+    --data KMMVisMath \
     --model Gemma3-12B \
     --mode all \
     --work-dir /workspace/project/kor_math/github_product/VLMEvalKit/inference_results/chartqa \
